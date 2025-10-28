8552b3ea5632b62050956cec2bac35790ae9b6bb
erjeric <jayamahendra32@gmail.com>
2025-10-28 17:22:07 +0700

    first commit

---
+++ Controllers/PengunjungController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class PengunjungController : Controller // 🟢 harus mewarisi Controller
+    {
+        static List<Pengunjung> data = new List<Pengunjung>();
+
+        public ActionResult Index()
+        {
+            return View(data);
+        }
+
+        [HttpPost]
+        public JsonResult Tambah(string nama, string kategori)
+        {
+            if (!string.IsNullOrWhiteSpace(nama) && !string.IsNullOrWhiteSpace(kategori))
+            {
+                var item = new Pengunjung
+                {
+                    ID = Guid.NewGuid().ToString(),
+                    Nama = nama,
+                    Kategori = kategori,
+                    TanggalKunjungan = DateTime.Now
+                };
+                data.Add(item);
+                return Json(new { success = true, pengunjung = item });
+            }
+            return Json(new { success = false });
+        }
+    }
+}
---
+++ Models/Pengunjung.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class Pengunjung
+    {
+        public string ID { get; set; }
+        public string Nama { get; set; }
+        public string Kategori { get; set; }
+        public DateTime TanggalKunjungan { get; set; }
+    }
+}
